f1598378b61c006df111c8a5c8e691b0ca9fc6c7
Bryan Smith <bryan.gsmith33@gmail.com>
2017-12-06 16:51:39 -0700

    accidentally had partial solutions. removed them

--- assessment.js
+++ assessment.js
@@ -1,9 +1,5 @@
 // #1 Create an object called 'me' that has three keys, 'firstname' 'age' and 'state'. Assign the keys the appropriate values.
-var me = {
-  firstname: 't',
-  age: 28,
-  stat: 'Ut'
-}
+
 // CODE HERE
 
 // #2 Add a new key/value pair to the me object using dot notation. The new key should be 'favecolor' and set it to your favorite color as a string
@@ -37,19 +33,11 @@ var myArray = [1,2,3,4]
 //DON'T TOUCH THE CODE ABOVE
 // CODE HERE
 
-// #8 Below is a array, myNumbers. create a function called 'evensOnly' that returns the 'evensArray' of only even numbers
+// #8 Below is a array, myNumbers. Create a function called 'evensOnly' that returns the 'evensArray' of only even numbers
 //DON'T TOUCH THE CODE BELOW
 var myNumbers = [333,1,4,5,511,34,88,77,222]
 //DON'T TOUCH THE CODE ABOVE
-var evensOnly = function(){
-  var evensArray = []
-  for(var i = 0; i < myNumbers.length; i++){
-    if(myNumbers[i] % 2 === 0){
-      evensArray.push(myNumbers[i])
-    }
-  }
-  return evensArray
-}
+
 // CODE HERE
 // #9 Using filter(), return only your friends of the array of people below. Assign it to a variable called 'trueFriends'.
 // CODE HERE
@@ -62,16 +50,6 @@ var peopleIknow = [
   {name: "Holly", friend: true}
 ]
 
-let trueFriends = peopleIknow.filter(people => people.friend)
-console.log(trueFriends);
-
 // #10 create a function caled indexFinder and loop ove the randomNumbers array below and return a new array called 'indexes' with just their indexes. Be sure to invoke indexFinder.
 let randomNumbers = [1,3453,34,456,32,3,2,0]
-let indexes = []
-let indexFinder = () => {
-  for(var i = 0; i < randomNumbers.length; i++){
-    indexes.push(i)
-  }
-  return indexes
-}
-indexFinder()
+
